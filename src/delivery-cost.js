a35d3114c2dc2c7bd0ccc287e985974f6bacd34a
ChinJin <cjcheok@gmail.com>
2022-08-18 10:05:25 +0800

    Initial setup commit.

---
+++ src/delivery-cost.js
@@ -0,0 +1,7 @@
+
+
+class DeliveryCost{
+
+}
+
+module.exports = DeliveryCost;
---
+++ test/delivery-cost.test.js
@@ -0,0 +1,9 @@
+
+const DeliveryCost = require('../src/delivery-cost');
+
+describe("Delivery Cost Tests", () => {
+
+    test("Test", async () => {
+        let dc = new DeliveryCost();
+    });
+});
